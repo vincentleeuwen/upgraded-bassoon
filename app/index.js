8dd4bf892a500686052e4c5154b69f90786f4780
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-06-23 15:18:35 +0200

    green bg working

--- app/index.js
+++ app/index.js
@@ -9,14 +9,12 @@ require('./index.css');
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello World!
-      </div>
-    );
+      <div>Hello World!</div>
+    )
   }
 }
 
 ReactDOM.render(
   <App />,
-  document.getElementById('app');
-); 
+  document.getElementById('app')
+);
