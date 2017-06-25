ee7d991b690a07866fe83e313f5673da9be45fd7
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-06-25 17:38:30 +0200

    refactor stateless comonent to function

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,26 +1,25 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 
-class SelectLanguage extends React.Component {
-  render() {
-    var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
-    return (
-      <ul className='languages'>
-        {
-          languages.map((lang) => {
-            return (
-              <li
-                style={lang === this.props.selectedLanguage ? { color: '#d0021b'} : null}
-                onClick={this.props.onSelect.bind(null, lang)}
-                key={lang}>
-                {lang}
-              </li>
-            )
-          }
-        )}
-      </ul>
-    )
-  }
+function SelectLanguage(props) {
+  var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
+
+  return (
+    <ul className='languages'>
+      {
+        languages.map((lang) => {
+          return (
+            <li
+              style={lang === props.selectedLanguage ? { color: '#d0021b'} : null}
+              onClick={props.onSelect.bind(null, lang)}
+              key={lang}>
+              {lang}
+            </li>
+          )
+        }
+      )}
+    </ul>
+  )
 }
 
 SelectLanguage.propTypes = {
