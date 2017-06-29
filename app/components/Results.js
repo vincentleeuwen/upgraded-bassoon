7ea8e00d2d062a2abd9de7312fcb196c34011efb
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-06-29 23:46:19 +0200

    update Results component

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,9 +1,67 @@
 var React = require('react');
+var queryString = require('query-string');
+var api = require('../utils/api');
+var Link = require('react-router-dom').Link;
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    }
+  }
+  componentDidMount() {
+    var players = queryString.parse(this.props.location.search);
+    console.log(players);
+    api.battle([
+      players.playerOneName,
+      players.playerTwoName
+    ]).then(function(results){
+      // console.log(results);
+      if (results === null) {
+        return this.setState(function(){
+          return {
+            error: 'Looks like there was an error. Please check that both users are on Github.',
+            loading: false
+          }
+        });
+      }
+
+      this.setState(function(){
+        return {
+          error: null,
+          winner: results[0],
+          loser: results[1],
+          loading: false
+        }
+      })
+    }.bind(this));
+  }
   render() {
+    var error = this.state.error;
+    var winner = this.state.winner;
+    var loser = this.state.loading;
+    var loading = this.state.loading;
+
+    if (loading === true) {
+      return <p>Loading...</p>
+    }
+
+    if (error) {
+      return (
+        <div>
+          <p>{error}</p>
+          <Link to="/battle">Reset</Link>
+        </div>
+      )
+    }
+
     return (
-      <p>Results</p>
+      <p>{JSON.stringify(this.state, null, 2)}</p>
     )
   }
 }
